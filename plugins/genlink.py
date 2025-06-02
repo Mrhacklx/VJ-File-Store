7742e9b67889a240808b7d43e9f9d8fd1fd72b59
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-06-02 12:25:58 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -41,7 +41,7 @@ async def incoming_gen_link(bot, message):
     if WEBSITE_URL_MODE == True:
         share_link = f"{WEBSITE_URL}?Tech_VJ={outstr}"
     else:
-        share_link = f"https:/t.me/{username}?start={outstr}"
+        share_link = f"https://t.me/{username}?start={outstr}"
     if user["base_site"] and user["shortener_api"] != None:
         short_link = await get_short_link(user, share_link)
         await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🖇️ sʜᴏʀᴛ ʟɪɴᴋ :- {short_link}</b>")
